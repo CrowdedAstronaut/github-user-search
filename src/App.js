b1cb1acf45a0e340e8396a3e3f5db67275df53fe
CrowdedAstronaut <ross@johnrossphillips.com>
2022-05-19 11:58:34 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import SearchForm from "./components/SearchForm/SearchForm";
+
+function handleChange(event) {
+  // setSearchString(event.target.value);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  // getImages(searchString);
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="site-title">devfinder</h1>
+      <SearchForm />
     </div>
   );
 }
---
+++ src/components/SearchForm/SearchForm.js
@@ -0,0 +1,25 @@
+import { SearchIcon } from "../AppIcons/AppIcons";
+import search from "../../assets/icons/icon-search.svg";
+
+export default function SearchForm({
+  handleChange,
+  handleSubmit,
+  searchString,
+}) {
+  return (
+    <form
+      onSubmit={handleSubmit}
+      className="form-horizontal"
+    >
+      <input
+        type="text"
+        placeholder="Search Github username..."
+        name="searchString"
+        required
+        onChange={handleChange}
+        value={searchString}
+      />{" "}
+      <button type="submit"> Search </button>
+    </form>
+  );
+}
